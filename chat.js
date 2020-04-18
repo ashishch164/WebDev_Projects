76b99ac00e8bc0e12ca96990f8d163356ff298d8
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-04-18 17:26:18 +0530

    WebchatBox-Public

---
+++ chat.js
@@ -0,0 +1,49 @@
+
+
+// window.onload = function () {
+//     let btnBeep = this.document.getElementById('beep')
+
+//     btnBeep.onclick = function () {
+//         socket.emit('beep')
+//     }
+
+// }
+
+
+const socket = io()
+
+$(() => {
+    $('#beep').mousedown(() => {
+      socket.emit('beep', { state: 'on' })
+    })
+    $('#beep').mouseup(() => {
+      socket.emit('beep', { state: 'off' })
+    })
+  
+    $('#send').click(() => {
+      let msg = $('#msg').val()
+      if (msg) {
+        socket.emit('msg_send', { msg: msg })
+        $('#msg').val('')
+      }
+    })
+  
+    socket.on('msg_rcvd', data => {
+      $('#chatbox').append($('<div>').text(data.msg))
+    })
+  
+    socket.on('beep', data => {
+      let status = $('#' + data.id)
+      if (!status[0]) {
+        $('#statusbox').append(
+          $(`<div class="status" id="${data.id}">`).text(data.id)
+        )
+        status = $('#' + data.id)
+      }
+  
+      status.css(
+        'background-color',
+        data.state == 'on' ? 'green' : 'red'
+      )
+    })
+  })
