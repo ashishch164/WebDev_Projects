6d3feadb54b018509c36f62920c5dd595e1803dd
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-07-27 13:30:00 +0530

    Update script.js

--- TodoList/script.js
+++ TodoList/script.js
@@ -9,26 +9,30 @@ function newItem() {
     // store();
   }
   
-  document.body.onkeyup = function(e) {
-    if (e.keyCode == 13) {
+
+  // The onkeyup event occurs when the user releases a key (on the keyboard).Execute a JavaScript when a user releases a key:
+  document.body.onkeyup = function(e) {    //jab user task likhne ke baad enter maare tab bhi kaam karna chahiye
+    if (e.keyCode == 13) {   //ENTER ka key code 13 hota h,to jaise hi user enter pr click krega,newitem() call hoga aur list me add krdega
       newItem();
-      store();
+      
     }
-  };
+  }
   
   function removeItem(e) {
     e.target.parentElement.removeChild(e.target);
   }
   function removeAll(){
-    var lst = document.getElementsByTagName("ul");
-      lst[0].innerHTML = "";
+    var lst = document.getElementsByTagName("ul");     //Get all elements in the document with the specified tag name:"ul"
+      lst[0].innerHTML = "";   //jab miljaaye wo to un sbko empty krdo,isse puri ul delete hojaayegi
   }
-  btn.onclick = function () {    
+  btn.onclick = function () {      //jab ADD vaale pe click hoga tb newitem() call krdena and jab ClearAll tab removeall() vaala
     newItem();
 }
 
 clear.onclick = function () {    
     removeAll();
 }
+
+
 
 
