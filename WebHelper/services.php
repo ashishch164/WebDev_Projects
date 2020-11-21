7083a49cd70ae7f863275de21d864528528545b0
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-11-21 13:35:36 +0530

    Create services.php

---
+++ WebHelper/services.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/gif/png" href="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg">
+	<title>Services</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<script src="script.js"></script>
+	<link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+	
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand text-primary font-weight-bold" href="#"><img src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="100"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="about.php">About Us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="mission.php">Our Mission</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" style="font-weight:bolder"href="services.php">Services</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://github.com/ashishch164/WebDev_Projects">Previous Projects</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="join.php">Join Us</a>
+			</li>
+		</ul>
+	</div>
+</div>	
+</nav>
+<!-- End Navigation -->
+
+<!-- Quote -->
+<div class="container-fluid">
+  <div class="row jumbotron">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+  <blockquote class="blockquote">
+    <p>"We don't want to push our ideas on to customers, we simply want to make what they want"</p>
+    <footer class="blockquote-footer">From Laura Ashley</footer>
+  </blockquote>
+	</div>
+  </div>
+</div>
+<!-- End Quote -->
+
+<!-- Our Services -->
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-lg-6 offset-lg-1">
+			<img src="img/m10.png" class="img-fluid rounded-circle">
+		</div>  		
+		<div class="col-md-12 col-lg-4">
+			<h1 class="display-4 text-center">Our Services</h1>
+<hr width="150px">
+			<p>Our mission is to provide businesses with the perfect website they are looking for, and that is what we do.
+We build websites, which our customers can rely on, and also provide regular updates and bug fixes.
+So if you need a website for your grocery outlet, food store, medical store, business startup, educational institution, or anything you want to take online, we are here for you.</p>
+		</div>  
+	</div>  
+</div>
+<!-- End Our Services -->
+
+<!--- Background image -->
+<figure>
+	<div class="fixed-wrap">
+		<div id="fixed">
+		</div>	
+	</div>
+</figure>
+<!--- End Background image -->
+
+<!--- Worth of products -->
+<div class="container-fluid padding bg-light">
+<div class="row welcome text-center">
+	<div class="col-12">
+		<h1 class="display-4">The Worth of our Products</h1>
+<hr width="150px">
+	</div>
+</div>
+</div>
+
+<div class="container-fluid padding bg-light">
+<div class="row padding">
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-2">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Superior Quality</h2>
+	</div>
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-0">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Fully Functional</h2>
+	</div>
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-2">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Convenience</h2>
+	</div>
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-0">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Absolutely Reliable</h2>
+	</div>
+</div>
+</div>
+<!--- End Worth of products -->
+          
+<!--- Footer -->
+<footer>
+<div class="container-fluid padding">
+<div class="row text-center font-weight-bold">
+	<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+		<hr class="light">
+		<h5>WebHelper Office</h5>
+		<hr class="light">
+		<p>Palam Colony,New Delhi-45,India</p>
+	</div>
+	<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+		<hr class="light">
+		<a class="logo text-light" href="#"><img src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="25"> WebHelper</a>
+		<hr class="light">
+		<p><a href="mailto: ashishkr737@gmail.com">ashishkr737@gmail.com</a></p>
+</div>
+<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+	<hr class="light">
+	<h5>Contact Us</h5>
+	<hr class="light">
+	<p>Mobile: +91 9711783764, +91 9205053495</p>
+	
+</div>
+<div class="col-12">
+	<hr class="light-100">
+	<h5> © 2020 Copyright: WebHelper</h5>
+</div>
+
+</div>
+</div>
+<button class="btn btn-primary scroll-top" data-scroll="up" type="button">
+<i class="fa fa-chevron-up"></i>
+</button>
+</footer>
+<!--- End Footer -->
+
+</body>
+</html>
