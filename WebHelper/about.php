a1b659c18202cfab3960fe0f18a909facb142b64
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-11-21 13:36:02 +0530

    Create about.php

---
+++ WebHelper/about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/gif/png" href="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg">
+	<title>About</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+		<script src="script.js"></script>
+	<link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand text-primary font-weight-bold" href="#"><img src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="100"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" style="font-weight:bolder"  href="about.php">About Us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="mission.php">Our Mission</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="services.php">Services</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://github.com/ashishch164/WebDev_Projects">Previous Projects</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="join.php">Join Us</a>
+			</li>
+		</ul>
+	</div>
+</div>	
+</nav>
+<!-- End Navigation -->
+
+<!--- Meet the team -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center bg-primary">
+		<div class="col-12">
+			<h1 class="display-4 text-white">About Us</h1>
+			<hr class="a" width="150px">
+		</div>  
+	</div>  
+</div>
+<!--- End Meet the team -->
+
+<!--- Cards -->
+<section id="contact-us" class="page-section">
+	<div class="container ">
+		<div class="row  ">
+			<div class="col-sm-6 col-md-11" >
+				
+				
+				<p class="centre">As everything is digitalizing in this era, businesses need an online handle too. Where everything is accessible on the click of two fingers, we aim to provide people with websites, so they can take their business online to expand their reach and accessibility. 
+					
+					</p>
+			</div>
+
+			<div class="card-deck">
+				<div class="card">
+					
+				  <img class="card-img-top" src="img/ashish.jpeg" alt="Card image cap" >
+				  <div class="card-block">
+					 
+					<h2 class="card-title" >Ashish Kumar</h2>
+					<h6 class="card-subtitle ">Co-Founder</h6><br>
+
+				<center><a href="https://www.linkedin.com/in/ashish-k-7a0291188/" target="_blank" class="btn btn-primary">See Profile</a></center>
+					
+				  </div>
+				</div>
+				<div class="card">
+				  <img class="card-img-top" src="img/WhatsApp Image 2020-10-08 at 5.11.52 PM.jpeg"  alt="Card image cap"  >
+				  <div class="card-block">
+					<h2 class="card-title">Anmol Yadav </h2>
+
+					<h6 class="card-subtitle ">Director executive</h6><br>
+					
+					<center>	<a href="https://www.instagram.com/yadav_aanmol/?igshid=eacdvrlq3sro/" target="_blank" class="btn btn-primary"  > See Profile</a></center>
+					
+				  </div>
+				</div>
+
+	
+</section>
+<br>
+<!--- End Cards -->
+
+
+
+<!--- Footer -->
+<footer>
+<div class="container-fluid padding">
+<div class="row text-center font-weight-bold">
+
+	<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+		<hr class="light">
+		<h5>WebHelper Office</h5>
+		<hr class="light">
+		<p>Palam Colony,New Delhi-45,India</p>
+	</div>
+	<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+		<hr class="light">
+		<a class="logo text-light" href="#"><img src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="25"> WebHelper</a>
+		<hr class="light">
+		<p><a href="mailto: ashishkr737@gmail.com">ashishkr737@gmail.com</a></p>
+</div>
+<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+	<hr class="light">
+	<h5>Contact Us</h5>
+    <hr class="light">
+    
+	<p>Mobile: +91 9711783764, +91 9205053495</p>
+	
+</div>
+<div class="col-12">
+	<hr class="light-100">
+	<h5> © 2020 Copyright: WebHelper</h5>
+   
+</div>
+</div>
+</div>
+<button class="btn btn-primary scroll-top" data-scroll="up" type="button">
+<i class="fa fa-chevron-up"></i>
+</button>
+</footer>
+<!--- End Footer -->
+
+</body>
+</html>
