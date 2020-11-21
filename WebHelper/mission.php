0c1331a852801c90f6be9005c6fcd1ca52c2a76d
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-11-21 13:42:28 +0530

    Create mission.php

---
+++ WebHelper/mission.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/gif/png" href="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg">
+	<title>Mission</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<script src="script.js"></script>
+	<link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<a class="navbar-brand text-primary font-weight-bold" href="#"><img src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="100"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="about.php">About Us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link active" style="font-weight:bolder" href="mission.php">Our Mission</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="services.php">Services</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://github.com/ashishch164/WebDev_Projects">Previous Projects</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="join.php">Join Us</a>
+			</li>
+		</ul>
+	</div>
+</div>	
+</nav>
+<!-- End Navigation -->
+
+<!-- Quote -->
+<div class="container-fluid">
+  <div class="row jumbotron">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+  <blockquote class="blockquote">
+    <p>"Websites promote you 24x7. No employee would do that"</p>
+    <footer class="blockquote-footer">Paul Cookson</footer>
+  </blockquote>
+	</div>
+  </div>
+</div>
+<!-- End Quote -->
+
+<!-- Our Mission -->
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-lg-6">
+			<img src="img/t4.png" class="img-fluid rounded mx-auto d-block">
+		</div>  		
+		<div class="col-md-12 col-lg-4">
+			<h1 class="display-4 text-center">Our Mission</h1>
+<hr width="200px">
+			<p>We aim to provide a website for an emerging startup, which aims to provide people with good quality websites and provide a user-friendly experience throughout the process.
+				Websites have a major role in boosting the reach and feel of a business, and we understand that. In addition to that, we want to provide people with a one stop destination for everything they need their website to do.
+				You need  it, and we make it for you
+				</p>
+		</div>  
+	</div>  
+</div>
+<!-- End Our Mission -->
+
+<!--- Background image -->
+<!--figure>
+	<div class="fixed-wrap">
+		<div id="fixed">
+		</div>	
+	</div>
+</figure-->
+<!--- End Background image -->
+
+<!-- Our Values -->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<div class="col-12">
+		<h1 class="display-4">Our Values</h1>
+<hr width="200px">
+	</div>
+	<div class="col-12">
+		<p class="lead">WebHelper is a values-driven company. Our values guide the perspective of the organization as well as our actions.</p>
+	</div>
+</div>
+</div>
+
+<div class="container-fluid padding">
+<div class="row padding">
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-2">
+		<img class="float-md-left float-lg-right worth" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Accountability</h2>
+		<p>Accountability is important these days, and our managers take the responsibility to give excellent services to the customers.</p>
+	</div>
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-0">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Diligence</h2>
+		<p>WebHelper is extremely diligent in customer satisfaction. We carefully look for our customer needs and requirements.</p>
+	</div>
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-2">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Perseverance</h2>
+		<p><p>We have intense intellectual curiosity and extraordinary powers of perseverance.</p>
+	</div>
+	<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 offset-xl-0">
+		<img class="float-md-left float-lg-right" src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="70">
+	</div>
+	<div class="col-xs-11 col-sm-5 col-md-5 col-lg-5 col-xl-3">
+		<h2 class="worth">Integrity</h2>
+		<p><p>Employees at WebHelper have the highest personal integrity and we are keen to preserve our integrity.</p>
+	</div>
+</div>
+</div>
+<!-- End Our Values -->          
+
+<!--- Footer -->
+<footer>
+<div class="container-fluid padding">
+<div class="row text-center font-weight-bold">
+
+	<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+		<hr class="light">
+		<h5>WebHelper Office</h5>
+		<hr class="light">
+		<p>Palam Colony,New Delhi-45,India</p>
+	</div>
+	<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+		<hr class="light">
+		<a class="logo text-light" href="#"><img src="img/WhatsApp Image 2020-10-07 at 11.27.49 PM.jpeg" height="25"> WebHelper</a>
+		<hr class="light">
+		<p><a href="mailto: ashishkr737@gmail.com">ashishkr737@gmail.com</a></p>
+</div>
+<div class="col-sm-10 offset-sm-1 col-md-4 offset-md-0">
+	<hr class="light">
+	<h5>Contact Us</h5>
+	<hr class="light">
+	<p>Mobile: +91 9711783764, +91 9205053495</p>
+	
+</div>
+<div class="col-12">
+	<hr class="light-100">
+	<h5> © 2020 Copyright: WebHelper</h5>
+</div>
+
+</div>
+</div>
+<button class="btn btn-primary scroll-top" data-scroll="up" type="button">
+<i class="fa fa-chevron-up"></i>
+</button>
+</footer>
+<!--- End Footer -->
+
+</body>
+</html>
