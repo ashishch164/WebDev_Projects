47cb52dc796e2b9aaa514ba6d946f5e841311df4
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-11-22 13:42:54 +0530

    Update script.js

--- WebHelper/script.js
+++ WebHelper/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('.scroll-top').fadeIn();
+    if ($(this).scrollTop() > 100) {  //check if page is scrolled greater than 100px
+      $('.scroll-top').fadeIn();    //if this happens, fadein the button
     } else {
       $('.scroll-top').fadeOut();
     }
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
   $('.scroll-top').click(function () {
     $("html, body").animate({
-      scrollTop: 0
+      scrollTop: 0    //when user clicks on button, scroll to top of the page to 0px
     }, 100);
       return false;
   });
