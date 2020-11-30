2cba53039e7598c17c5c5ed9c4ed12f169c0cc2b
ASHISH KUMAR <42812707+ashishch164@users.noreply.github.com>
2020-11-30 13:40:11 +0530

    Add files via upload

---
+++ Railway Reservation System/customer/homepage.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+if($_SESSION['n']=='')
+{
+  header("location:index.php");
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link rel="stylesheet" href="homepage.css">
+    <title>HomePage</title>
+    <script>
+      function myFunction() {
+          
+          if (window.confirm('You logout successfully. Redirecting to Login Page...'))
+  {
+     
+      window.open("index.php");
+  }
+  else
+  {
+      die();
+  }
+   
+      }
+  </script>
+   <!--    <script type="text/javascript">
+      $(window).on('load',function(){
+          setTimeout(function(){ alert(" Login succesfully"); }, 700); 
+      });
+  </script>-->
+
+</head>
+
+<body>
+    <div class="container">
+        <section class="Page-Top" width=100%>
+            <!--<marquee behavior="scroll" direction="left" scrollamount="10">-->
+                <div class="Heading head">
+                    <img id="logo" src="favicon.png" alt="logo"height="100" width="120">
+                    <div class="Heading-Text">Railway Reservation System</div>
+                </div>
+            </marquee>
+        </section>
+
+ 
+
+<marquee behavior="scroll" class="HeadLine"scrollamount="10" direction="left">Due to COVID-19, only special trains are running. Stay Home, Stay Safe!</marquee>
+        
+
+<!-- Navigation bar-->
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary" >
+           
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#"style="font-weight: bold">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="VTdetail.php">View Train Details</a>
+                <a class="nav-item nav-link" href="booking.php">Book Tickets</a>
+                <a class="nav-item nav-link" href="booking_history.php">Booking History</a>
+                <a class="nav-item nav-link" href="cancellation.php">Cancellation</a>
+
+               <!-- <a class="nav-item nav-link"href= "updateProfile.html">Update Profile</a> -->
+               <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Update Profile
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="updateProfile.php  ">Edit Profile</a>
+                  <a class="dropdown-item" href="reset-paswd.php">Change password</a>
+                  <div class="dropdown-divider"></div>
+                  
+                </div></li>
+
+               
+                
+               <a class="nav-item nav-link"   href="logout.php">Logout</a>
+                
+              </div>
+            </div>
+          </nav>
+
+        <!--- Image Slider           -->
+
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="Passenger-train-at-Jalgaon-Junction-railway-station.jpg"height="503px" width="1400px" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="wp5522042.jpg" height="503px" width="1400px"alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="New Picture (56).bmp"height="503px" width="1400px" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+
+
+
+
+          <!-- FOOTER-->
+        <section class="background">
+
+        </section>
+
+        <footer class="footer">
+            <div class="footer-text">
+                 © 2020 Copyright: All Rights Reserved
+            </div>
+        </footer>
+    </nav>
+    </div>
+</body>
+
+</html>
